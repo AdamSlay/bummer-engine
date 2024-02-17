6a04c08a4484b3d1921c31301f19f6924243c1fb
Adam Slay <aslay@mesonet.org>
2024-02-17 17:01:52 -0600

    Use TTF_RenderUTF8_Blended_Wrapped() for rendering text without it being pixelated

--- src/UI/Menu.cpp
+++ src/UI/Menu.cpp
@@ -12,7 +12,8 @@ void start_menu(SDL_Renderer* renderer, TTF_Font* font) {
     // Set up the surface for the text
     std::string text = "Bummer Engine";
     SDL_Color color = {255, 245, 140};
-    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text.c_str(), color);
+    int wrapLength = 0;  // 0 means only wrap on newlines
+    SDL_Surface* textSurface = TTF_RenderUTF8_Blended_Wrapped(font, text.c_str(), color, wrapLength);
     if (textSurface == nullptr) {
         std::cout << "Unable to render text surface! SDL_ttf Error: " << TTF_GetError() << std::endl;
         return;
@@ -31,7 +32,7 @@ void start_menu(SDL_Renderer* renderer, TTF_Font* font) {
     SDL_FreeSurface(textSurface);
 
     // Set the position of the text then create a rectangle to render the text to
-    int x = (SCREEN_WIDTH / 2) - (textWidth / 2);
+    int x = (SCREEN_WIDTH / 2) - (textWidth / 2);  // Center the text
     int y = (SCREEN_HEIGHT / 2) - (textHeight / 2);
     SDL_Rect renderQuad = {x, y, textWidth, textHeight};
 
