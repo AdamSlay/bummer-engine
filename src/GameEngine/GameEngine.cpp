e0ec1d0565ac461650363e87c74a636c4924a3a4
Adam Slay <aslay@mesonet.org>
2024-02-19 15:00:23 -0600

    Refactor game_loop(): create poll_events() and render_splash_screen()

--- src/GameEngine/GameEngine.cpp
+++ src/GameEngine/GameEngine.cpp
@@ -9,36 +9,50 @@ void game_loop(SDL_Renderer* renderer, TTF_Font* font) {
      * @param renderer: The SDL renderer
      * @param font: The TTF font
      */
+    // Render the splash screen on startup
+    render_splash_screen(renderer, font);
 
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        // Poll events
+        poll_events(e, quit);
+
+        // Clear the renderer
+        SDL_SetRenderDrawColor(renderer, 104,102,182, 0xFF);
+        SDL_RenderClear(renderer);
+
+        // Render the screen
+        SDL_RenderPresent(renderer);
+    }
+}
+
+void render_splash_screen(SDL_Renderer* renderer, TTF_Font* font) {
     SDL_SetRenderDrawColor(renderer, 104,102,182, 0xFF);
     SDL_RenderClear(renderer);
     splash_screen(renderer, font);
     SDL_RenderPresent(renderer);
     SDL_Delay(5000);
+}
 
-    SDL_Event e;
-    bool splash = false;
-    bool quit = false;
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-            else if (e.type == SDL_KEYDOWN) {
-                switch (e.key.keysym.sym) {
-                    case SDLK_s:
-                        return;
-                    case SDLK_q:
-                        exit(0);
-                }
+void poll_events(SDL_Event& e, bool& quit) {
+    /**
+     * Polls events and sets the quit flag if the user closes the window
+     *
+     * @param e: The SDL event
+     * @param quit: The quit flag
+     */
+    while (SDL_PollEvent(&e) != 0) {
+        if (e.type == SDL_QUIT) {
+            quit = true;
+        }
+        else if (e.type == SDL_KEYDOWN) {
+            switch (e.key.keysym.sym) {
+                case SDLK_s:
+                    return;
+                case SDLK_q:
+                    exit(0);
             }
         }
-        // Clear the renderer
-        SDL_SetRenderDrawColor(renderer, 104,102,182, 0xFF);
-        SDL_RenderClear(renderer);
-
-
-        // Render the screen
-        SDL_RenderPresent(renderer);
     }
 }
--- src/GameEngine/GameEngine.h
+++ src/GameEngine/GameEngine.h
@@ -5,5 +5,7 @@
 #include <SDL2/SDL_ttf.h>
 
 void game_loop(SDL_Renderer* renderer, TTF_Font* font);
+void render_splash_screen(SDL_Renderer* renderer, TTF_Font* font);
+void poll_events(SDL_Event& e, bool& quit);
 
 #endif //BUMMERENGINE_GAMEENGINE_H
