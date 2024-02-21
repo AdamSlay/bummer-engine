64096f6ce83aa7c714ad04cf6f983e70b2782a9b
Adam Slay <aslay@mesonet.org>
2024-02-21 09:27:43 -0600

    Stand up EntityManager skeleton

--- src/ECS/EntityManager.cpp
+++ src/ECS/EntityManager.cpp
@@ -1,40 +1,16 @@
-#ifndef BUMMERENGINE_ENTITYMANAGER_H
-#define BUMMERENGINE_ENTITYMANAGER_H
+#include "EntityManager.h"
 
-#include <vector>
-#include <unordered_map>
-#include <typeindex>
+template <typename T>
+void Entity::addComponent(T component) {
+    components[std::type_index(typeid(T))] = component;
+}
 
-class Entity
-{
-public:
-    template <typename T>
-    void addComponent(T component)
-    {
-        components[std::type_index(typeid(T))] = component;
-    }
+template <typename T>
+T& Entity::getComponent() {
+    return components[std::type_index(typeid(T))];
+}
 
-    template <typename T>
-    T& getComponent()
-    {
-        return components[std::type_index(typeid(T))];
-    }
-
-private:
-    std::unordered_map<std::type_index, void *> components;
-};
-
-class EntityManager
-{
-public:
-    Entity& createEntity()
-    {
-        entities.emplace_back();  // similar to push_back, but constructs the object in place
-        return entities.back();
-    }
-
-private:
-    std::vector<Entity> entities;
-};
-
-#endif // BUMMERENGINE_ENTITYMANAGER_H
+Entity& EntityManager::createEntity() {
+    entities.emplace_back();
+    return entities.back();
+}
--- src/ECS/EntityManager.h
+++ src/ECS/EntityManager.h
@@ -1,11 +1,28 @@
-
 #ifndef BUMMERENGINE_ENTITYMANAGER_H
 #define BUMMERENGINE_ENTITYMANAGER_H
 
+#include <unordered_map>
+#include <typeindex>
 
-class EntityManager {
+class Entity {
+public:
+    template <typename T>
+    void addComponent(T component);
 
+    template <typename T>
+    T& getComponent();
+
+private:
+    std::unordered_map<std::type_index, void *> components;
 };
 
 
-#endif //BUMMERENGINE_ENTITYMANAGER_H
+class EntityManager {
+public:
+    Entity& createEntity();
+
+private:
+    std::vector<Entity> entities;
+};
+
+#endif // BUMMERENGINE_ENTITYMANAGER_H
