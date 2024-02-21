b9e0fa87db94398693eb33c82e69499efafcdfe2
Adam Slay <aslay@mesonet.org>
2024-02-21 09:34:08 -0600

    Documentation

--- src/ECS/EntityManager.cpp
+++ src/ECS/EntityManager.cpp
@@ -2,15 +2,26 @@
 
 template <typename T>
 void Entity::addComponent(T component) {
+    /**
+     * Add a component of type T to the entity
+     *
+     * @param component The component to add
+     */
     components[std::type_index(typeid(T))] = component;
 }
 
 template <typename T>
 T& Entity::getComponent() {
+    /**
+     * Return a reference to the component of type T
+     */
     return components[std::type_index(typeid(T))];
 }
 
 Entity& EntityManager::createEntity() {
+    /**
+     * Create a new entity, append it to the entities vector and return a reference to it
+     */
     entities.emplace_back();
     return entities.back();
 }
