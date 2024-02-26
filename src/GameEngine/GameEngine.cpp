d58b66e23629a89963a3b9352d8879c5906b12ea
Adam Slay <aslay@mesonet.org>
2024-02-26 15:27:56 -0600

    Splash

--- src/GameEngine/GameEngine.cpp
+++ src/GameEngine/GameEngine.cpp
@@ -12,7 +12,7 @@ void game_loop(SDL_Renderer* renderer, TTF_Font* font) {
      * @param renderer: The SDL renderer
      * @param font: The TTF font
      */
-//    render_splash_screen(renderer, font);
+    render_splash_screen(renderer, font);
 
     EntityManager entityManager;
     RenderSystem renderSystem;
