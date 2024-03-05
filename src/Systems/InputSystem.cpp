e484cbcc082d5dd9cb2c1c25adfd9b67f97caafc
Adam Slay <aslay@mesonet.org>
2024-03-05 16:15:29 -0600

    Quit immediately when InputSystem::update registers quit event

--- src/Systems/InputSystem.cpp
+++ src/Systems/InputSystem.cpp
@@ -1,13 +1,10 @@
 #include "InputSystem.h"
 #include <SDL2/SDL.h>
 
-void InputSystem::update(EntityManager &entityManager, bool &quit)
-{
+void InputSystem::update(EntityManager &entityManager, bool &quit) {
     // Clear justPressed for all entities
-    for (Entity &entity : entityManager.getEntities())
-    {
-        if (entity.hasComponent<Input>())
-        {
+    for (Entity &entity : entityManager.getEntities()) {
+        if (entity.hasComponent<Input>()) {
             Input &input = entity.getComponent<Input>();
             input.justPressed.clear();
             input.justReleased.clear();
@@ -15,26 +12,20 @@ void InputSystem::update(EntityManager &entityManager, bool &quit)
     }
 
     SDL_Event e;
-    while (SDL_PollEvent(&e) != 0)
-    {
-        if (e.type == SDL_QUIT)
-        {
+    while (SDL_PollEvent(&e) != 0) {
+        if (e.type == SDL_QUIT) {
             quit = true;
+            break;
         }
-        else if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP)
-        {
-            for (Entity &entity : entityManager.getEntities())
-            {
-                if (entity.hasComponent<Input>())
-                {
+        else if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
+            for (Entity &entity : entityManager.getEntities()) {
+                if (entity.hasComponent<Input>()) {
                     Input &input = entity.getComponent<Input>();
                     input.keyStates[e.key.keysym.scancode] = (e.type == SDL_KEYDOWN);
-                    if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
-                    {
+                    if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
                         input.justPressed[e.key.keysym.scancode] = true;
                     }
-                    else if (e.type == SDL_KEYUP && e.key.repeat == 0)
-                    {
+                    else if (e.type == SDL_KEYUP && e.key.repeat == 0) {
                         input.justReleased[e.key.keysym.scancode] = true;
                     }
                 }
