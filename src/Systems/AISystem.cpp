a5a2a7704e33ff7d04d5b05ccfccfe8fd26d2ee9
Adam Slay <aslay@mesonet.org>
2024-03-22 15:35:48 -0500

    Update AISytem if not stunned

--- src/Systems/AISystem.cpp
+++ src/Systems/AISystem.cpp
@@ -9,25 +9,24 @@
 void AISystem::update(EntityManager& entityManager) {
     for (Entity& entity : entityManager.getEntities()) {
         if (entity.hasComponent<AI>() && entity.hasComponent<Transform>() && entity.hasComponent<Velocity>()) {
-            AI& ai = entity.getComponent<AI>();
-            Transform& transform = entity.getComponent<Transform>();
-            Velocity& velocity = entity.getComponent<Velocity>();
-
-            // If the enemy is at the edge of the patrol range, reverse direction
-            if ((velocity.direction == 1 && transform.x >= ai.patrolStart.first + ai.patrolRange) ||
-                (velocity.direction == -1 && transform.x <= ai.patrolStart.first - ai.patrolRange)) {
-                velocity.direction *= -1;
-            }
-
-            // Move the enemy in the current direction
             State& state = entity.getComponent<State>();
             if (state.state != playerStates::BASIC_ATTACK && state.state != playerStates::HIT && state.state != playerStates::STUNNED) {
+                AI& ai = entity.getComponent<AI>();
+                Transform& transform = entity.getComponent<Transform>();
+                Velocity& velocity = entity.getComponent<Velocity>();
+
+                // If the enemy is at the edge of the patrol range, reverse direction
+                if ((velocity.direction == 1 && transform.x >= ai.patrolStart.first + ai.patrolRange) ||
+                    (velocity.direction == -1 && transform.x <= ai.patrolStart.first - ai.patrolRange)) {
+                    velocity.direction *= -1;
+                }
+
+                // Move the enemy in the current direction
                 entity.changeState(playerStates::RUN);
                 velocity.dx = 1 * velocity.direction;
-            }
-
-            if (entity.hasComponent<AttackMap>()) {
-                attack(entityManager, entity);
+                if (entity.hasComponent<AttackMap>()) {
+                    attack(entityManager, entity);
+                }
             }
         }
     }
