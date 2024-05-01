9f15434891e0c69eb61070ff4e787bf5792066fe
Adam Slay <aslay@mesonet.org>
2024-05-01 14:35:16 -0500

    Add tests for Utils

--- tests/Test_Utils.cpp
+++ tests/Test_Utils.cpp
@@ -1,7 +1,28 @@
 #include <gtest/gtest.h>
 #include "../src/Utils.h"
+#include "../src/ECS/Components.h"
 
 
-TEST(UtilsTest, Test1) {
-    EXPECT_EQ(1,1);
+TEST(UtilsTest, Test_stringToAction) {
+    Action jumpAction = Utils::stringToAction("JUMP");
+    EXPECT_EQ(jumpAction, Action::JUMP);
+
+    Action attackAction = Utils::stringToAction("ATTACK");
+    EXPECT_EQ(attackAction, Action::ATTACK);
+}
+
+TEST(UtilsTest, Test_actionToString) {
+    std::string jumpAction = Utils::actionToString(Action::JUMP);
+    EXPECT_EQ(jumpAction, "JUMP");
+
+    std::string waitAction = Utils::actionToString(Action::WAIT);
+    EXPECT_EQ(waitAction, "WAIT");
+}
+
+TEST(UtilsTest, Test_stringToDirection) {
+    Direction leftDirection = Utils::stringToDirection("LEFT");
+    EXPECT_EQ(leftDirection, Direction::LEFT);
+
+    Direction invalidDirection = Utils::stringToDirection("INVALID");
+    EXPECT_EQ(invalidDirection, Direction::STILL);
 }
