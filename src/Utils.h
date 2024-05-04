6e967ec89d14abe767adf8e9e78f9152ca8c91e5
Adam Slay <aslay@mesonet.org>
2024-05-04 17:37:52 -0500

    Update Utils header

--- src/Utils.h
+++ src/Utils.h
@@ -8,13 +8,14 @@
 namespace Utils {
     Action stringToAction(const std::string& action);
     Direction stringToDirection(const std::string& direction);
-    void publishEvent(std::string eventString, Entity* primaryEntity = nullptr, Entity* secondaryEntity = nullptr);
     std::string playerStateToString(playerStates state);
     std::string actionToString(Action action);
-    void setTransformPos(Entity& entity, int x, int y);
+
+    // TODO: These tests need to be moved to their respective test suites
+    void publishEvent(std::string eventString, Entity* primaryEntity = nullptr, Entity* secondaryEntity = nullptr);
+    SDL_Rect getHitboxRect(Hitbox& hitbox, Entity& entity);
     void render_collider(Entity &entity, SDL_Renderer *renderer);
     void render_all_colliders(EntityManager& entityManager, SDL_Renderer* renderer);
-    SDL_Rect getHitboxRect(Hitbox& hitbox, Entity& entity);
     void render_hitboxes(EntityManager& entityManager, SDL_Renderer* renderer);
 }
 #endif //BUMMERENGINE_UTILS_H
