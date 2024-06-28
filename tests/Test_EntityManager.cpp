5f4e1b1166b4bf2242cba3570263c7366202c350
Adam Slay <aslay@mesonet.org>
2024-06-28 16:00:57 -0500

    Add test for addComponentCollider, addComponentTransform

--- tests/Test_EntityManager.cpp
+++ tests/Test_EntityManager.cpp
@@ -62,3 +62,53 @@ TEST(EntityManagerTest, TestAddComponentNpc) {
     // Cleanup
     SDL_DestroyRenderer(renderer);
 }
+
+TEST(EntityManagerTest, TestAddComponentTransform) {
+    // Arrange
+    TextureManager textureManager;
+    SDL_Renderer* renderer = SDL_CreateRenderer(SDL_CreateWindow("", 0, 0, 0, 0, 0), -1, 0);
+    EntityManager entityManager(&textureManager, renderer);
+    Entity& entity = entityManager.createEntity();
+    nlohmann::ordered_json componentJson;
+    componentJson["x"] = 100;
+    componentJson["y"] = 200;
+    componentJson["scale"] = 1.0;
+
+    // Act
+    entityManager.addComponentTransform(entity, componentJson);
+
+    // Assert
+    ASSERT_TRUE(entity.hasComponent<Transform>());
+    ASSERT_EQ(entity.getComponent<Transform>().x, 100);
+    ASSERT_EQ(entity.getComponent<Transform>().y, 200);
+    ASSERT_EQ(entity.getComponent<Transform>().scale, 1.0);
+
+    // Cleanup
+    SDL_DestroyRenderer(renderer);
+}
+
+TEST(EntityManagerTest, TestAddComponentCollider) {
+    // Arrange
+    TextureManager textureManager;
+    SDL_Renderer* renderer = SDL_CreateRenderer(SDL_CreateWindow("", 0, 0, 0, 0, 0), -1, 0);
+    EntityManager entityManager(&textureManager, renderer);
+    Entity& entity = entityManager.createEntity();
+    nlohmann::ordered_json componentJson;
+    componentJson["offsetX"] = 10;
+    componentJson["offsetY"] = 20;
+    componentJson["width"] = 30;
+    componentJson["height"] = 40;
+
+    // Act
+    entityManager.addComponentCollider(entity, componentJson);
+
+    // Assert
+    ASSERT_TRUE(entity.hasComponent<Collider>());
+    ASSERT_EQ(entity.getComponent<Collider>().offsetX, 10);
+    ASSERT_EQ(entity.getComponent<Collider>().offsetY, 20);
+    ASSERT_EQ(entity.getComponent<Collider>().width, 30);
+    ASSERT_EQ(entity.getComponent<Collider>().height, 40);
+
+    // Cleanup
+    SDL_DestroyRenderer(renderer);
+}
