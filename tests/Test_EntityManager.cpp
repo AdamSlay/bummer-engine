947f3a698ed597e66affc938c3ae790f1bec11c2
Adam Slay <aslay@mesonet.org>
2024-06-28 15:59:19 -0500

    Add test for addComponentNpc, addComponentIntent

--- tests/Test_EntityManager.cpp
+++ tests/Test_EntityManager.cpp
@@ -20,3 +20,45 @@ TEST(EntityManagerTest, TestAddComponentPlayer) {
     // Cleanup
     SDL_DestroyRenderer(renderer);
 }
+
+TEST(EntityManagerTest, TestAddComponentIntent) {
+    // Arrange
+    TextureManager textureManager;
+    SDL_Renderer* renderer = SDL_CreateRenderer(SDL_CreateWindow("", 0, 0, 0, 0, 0), -1, 0);
+    EntityManager entityManager(&textureManager, renderer);
+    Entity& entity = entityManager.createEntity();
+    nlohmann::ordered_json componentJson;
+    componentJson["action"] = "MOVE_RIGHT";
+    componentJson["direction"] = "RIGHT";
+
+    // Act
+    entityManager.addComponentIntent(entity, componentJson);
+
+    // Assert
+    ASSERT_TRUE(entity.hasComponent<Intent>());
+    ASSERT_EQ(entity.getComponent<Intent>().action, Action::MOVE_RIGHT);
+    ASSERT_EQ(entity.getComponent<Intent>().direction, Direction::RIGHT);
+
+    // Cleanup
+    SDL_DestroyRenderer(renderer);
+}
+
+TEST(EntityManagerTest, TestAddComponentNpc) {
+    // Arrange
+    TextureManager textureManager;
+    SDL_Renderer* renderer = SDL_CreateRenderer(SDL_CreateWindow("", 0, 0, 0, 0, 0), -1, 0);
+    EntityManager entityManager(&textureManager, renderer);
+    Entity& entity = entityManager.createEntity();
+    nlohmann::ordered_json componentJson;
+    componentJson["type"] = "Alien";
+
+    // Act
+    entityManager.addComponentNpc(entity, componentJson);
+
+    // Assert
+    ASSERT_TRUE(entity.hasComponent<Npc>());
+    ASSERT_EQ(entity.getComponent<Npc>().type, "Alien");
+
+    // Cleanup
+    SDL_DestroyRenderer(renderer);
+}
